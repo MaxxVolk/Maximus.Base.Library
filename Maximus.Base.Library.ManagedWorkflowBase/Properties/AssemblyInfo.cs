a595e61e9d097ac6320943db7a02aa8c9adf0ee8
Maxim Volkov <maxim.volkov@revera.co.nz>
2021-03-31 14:46:10 +1300

    fixed RegistryHelper, added standalone support for the base workflow library

--- Maximus.Base.Library.ManagedWorkflowBase/Properties/AssemblyInfo.cs
+++ Maximus.Base.Library.ManagedWorkflowBase/Properties/AssemblyInfo.cs
@@ -4,11 +4,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if STANDALONE
+[assembly: AssemblyTitle("Maximus.Standalone.Library.ManagedWorkflowBase")]
+#else
 [assembly: AssemblyTitle("Maximus.Base.Library.ManagedWorkflowBase")]
+#endif
 [assembly: AssemblyDescription("Base managed module classes to create SCOM actions.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
+#if STANDALONE
+[assembly: AssemblyProduct("Maximus.Standalone.Library.ManagedWorkflowBase")]
+#else
 [assembly: AssemblyProduct("Maximus.Base.Library.ManagedWorkflowBase")]
+#endif
 [assembly: AssemblyCopyright("Copyright © Maximus 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +39,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+#if STANDALONE
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+#else
 [assembly: AssemblyVersion("1.0.1.0")] // to reference the library
 [assembly: AssemblyFileVersion("1.0.1.4")]
+#endif
--- Maximus.Base.Library.ManagedWorkflowBase/ServiceHelpers/RegistryHelper.cs
+++ Maximus.Base.Library.ManagedWorkflowBase/ServiceHelpers/RegistryHelper.cs
@@ -4,13 +4,41 @@ using Microsoft.Win32;
 
 namespace Maximus.Library.Helpers
 {
+  /// <summary>
+  /// Set of static helper methods to work with remote and local Registry.
+  /// <para>
+  /// 
+  /// </para>
+  /// </summary>
   public static class RegistryHelper
   {
+    /// <summary>
+    /// Read registry value as <seealso cref="string"/>.
+    /// Method doesn't check registry value type and cast all types to <seealso cref="string"/>.
+    /// </summary>
+    /// <param name="computerName">Host name of remoter computer. Use '.' or 'localhost' or empty or null string for local Registry access. </param>
+    /// <param name="fullPath">PowerShell style registry key path and value name in one path string. 
+    /// <br><i>Example: HKLM:\Software\Vendor\ValueName</i></br>
+    /// <br><b>Note:</b> value names with '\' character are not supported as used for path separator</br>
+    /// </param>
+    /// <param name="defaultValue">Default value to use if target path doesn't exist. When null, an <seealso cref="IOException"/> will be thrown, if path not found.</param>
+    /// <returns>A <seealso cref="string"/> value from Registry.</returns>
+    /// <exception cref="IOException">: when no default value is set and path is not found.</exception>
     public static string ReadRegistryString(string computerName, string fullPath, string defaultValue = null)
     {
       return ReadRegistryValue(computerName, fullPath, defaultValue).ToString();
     }
 
+    /// <summary>
+    /// Read target REG_DWORD value as Unix time (a number of seconds from 1st Jan 1970).
+    /// </summary>
+    /// <param name="computerName">Host name of remoter computer. Use '.' or 'localhost' or empty or null string for local Registry access. </param>
+    /// <param name="fullPath">PowerShell style registry key path and value name in one path string. 
+    /// <br><i>Example: HKLM:\Software\Vendor\ValueName</i></br>
+    /// <br><b>Note:</b> value names with '\' character are not supported as used for path separator</br>
+    /// </param>
+    /// <returns>A <seealso cref="DateTime"/> value converted from REG_DWORD</returns>
+    /// <exception cref="IOException">: when no default value is set and path is not found.</exception>
     public static DateTime ReadRegistryUnixTime(string computerName, string fullPath)
     {
       int rawResult = (int)ReadRegistryValue(computerName, fullPath, null);
@@ -18,32 +46,123 @@ namespace Maximus.Library.Helpers
       return ServiceHelper.FromUnixTime(unixTime);
     }
 
+    /// <summary>
+    /// Read REG_DWORD registry value.
+    /// </summary>
+    /// <param name="computerName">Host name of remoter computer. Use '.' or 'localhost' or empty or null string for local Registry access. </param>
+    /// <param name="fullPath">PowerShell style registry key path and value name in one path string. 
+    /// <br><i>Example: HKLM:\Software\Vendor\ValueName</i></br>
+    /// <br><b>Note:</b> value names with '\' character are not supported as used for path separator</br>
+    /// </param>
+    /// <param name="defaultValue">Default value to use if target path doesn't exist. When null, an <seealso cref="IOException"/> will be thrown, if path not found.</param>
+    /// <returns>Registry value.</returns>
+    /// <exception cref="IOException"></exception>
+    /// <exception cref="InvalidCastException"></exception>
     public static uint ReadRegistryUInt(string computerName, string fullPath, uint? defaultValue = null)
     {
-      int rawResult = (int)ReadRegistryValue(computerName, fullPath, defaultValue);
-      return unchecked((uint)rawResult);
+      // it should be like this, if not that complicated, then fail.
+      if (defaultValue == null)
+      {
+        object s = ReadRegistryValue(computerName, fullPath, null);
+        if (s is int intS)
+          return unchecked((uint)intS);
+        throw new InvalidCastException("Target Registry value was not of REG_DWORD type.");
+      }
+      else
+      {
+        object d = ReadRegistryValue(computerName, fullPath, defaultValue.Value); // to make default value uint, not uint?
+        // .Net read REG_DWORD as int
+        if (d.GetType() == typeof(int))
+        {
+          int x = (int)d;
+          return unchecked((uint)x);
+        }
+        // default result returned as uint
+        if (d.GetType() == typeof(uint))
+          return (uint)d;
+        throw new InvalidCastException("Target Registry value was not of REG_DWORD type.");
+      }
     }
 
+    /// <summary>
+    /// Read registry value as boolean. Numeric registry types REG_DWORD and REG_QWORD, and REG_SZ are supported.
+    /// For numeric values zero is read as <see langword="false"/>, and any non-zero as <see langword="true"/>.
+    /// For string values literals 'Yes'/'No' or 'True'/'False' are supported.
+    /// </summary>
+    /// <param name="computerName">Host name of remoter computer. Use '.' or 'localhost' or empty or null string for local Registry access. </param>
+    /// <param name="fullPath">PowerShell style registry key path and value name in one path string. 
+    /// <br><i>Example: HKLM:\Software\Vendor\ValueName</i></br>
+    /// <br><b>Note:</b> value names with '\' character are not supported as used for path separator</br>
+    /// </param>
+    /// <param name="defaultValue">Default value to use if target path doesn't exist. When null, an <seealso cref="IOException"/> will be thrown, if path not found.</param>
+    /// <returns>Boolean value based on content of target's registry value.</returns>
     public static bool ReadRegistryBoolean(string computerName, string fullPath, bool? defaultValue = null)
     {
-      int? defaultInt = null;
-      if (defaultValue != null)
-        defaultInt = defaultValue == true ? 1 : 0;
-      int RawResult = (int)ReadRegistryValue(computerName, fullPath, defaultInt);
-      return RawResult != 0;
+      object RawResult;
+      if (defaultValue == null)
+        RawResult = ReadRegistryValue(computerName, fullPath, null);
+      else
+        RawResult = ReadRegistryValue(computerName, fullPath, defaultValue.Value ? 1 : 0);
+
+      if (RawResult is int intResult)
+        return intResult != 0;
+      if (RawResult is long longResult)
+        return longResult != 0;
+      if (RawResult is string strResult)
+        return strResult.ToUpperInvariant() == "TRUE" || strResult.ToUpperInvariant() == "YES";
+
+      throw new InvalidCastException("Target Registry value was not of REG_QWORD, REG_DWORD, not REG_SZ types.");
     }
 
+    /// <summary>
+    /// Read REG_QWORD or REG_DWORD registry value.
+    /// </summary>
+    /// <param name="computerName">Host name of remoter computer. Use '.' or 'localhost' or empty or null string for local Registry access. </param>
+    /// <param name="fullPath">PowerShell style registry key path and value name in one path string. 
+    /// <br><i>Example: HKLM:\Software\Vendor\ValueName</i></br>
+    /// <br><b>Note:</b> value names with '\' character are not supported as used for path separator</br>
+    /// </param>
+    /// <param name="defaultValue">Default value to use if target path doesn't exist. When null, an <seealso cref="IOException"/> will be thrown, if path not found.</param>
+    /// <returns>Registry value.</returns>
+    /// <exception cref="IOException"></exception>
+    /// <exception cref="InvalidCastException"></exception>
     public static ulong ReadRegistryULong(string computerName, string fullPath, ulong? defaultValue = null)
     {
-      return unchecked((ulong)ReadRegistryValue(computerName, fullPath, defaultValue));
+      // it should be like this, if not that complicated, then fail.
+      if (defaultValue == null)
+      {
+        object s = ReadRegistryValue(computerName, fullPath, null);
+        if (s is int intS)
+          return unchecked((ulong)intS);
+        if (s is long longS)
+          return unchecked((ulong)longS);
+        throw new InvalidCastException("Target Registry value was not of REG_QWORD type.");
+      }
+      else
+      {
+        object d = ReadRegistryValue(computerName, fullPath, defaultValue.Value); // to make default value ulong, not ulong?
+        // .Net read REG_QWORD as long
+        if (d is int intS)
+          return unchecked((ulong)intS);
+        if (d is long longS)
+          return unchecked((ulong)longS);
+        // default result returned as ulong
+        if (d.GetType() == typeof(ulong))
+          return (ulong)d;
+        throw new InvalidCastException("Target Registry value was not of REG_QWORD type.");
+      }
     }
 
     /// <summary>
     /// Read registry parameter value from full path: HKLM:\Key\Parameter. The PowerShell-style key prefix can be replaced with its
     /// full name: HKEY_LOCAL_MACHINE
     /// </summary>
-    /// <param name="computerName">Remote computer name. Leave empty, null, '.' or 'localhost' to access local Registry.</param>
-    /// <param name="fullPath"></param>
+    /// <param name="computerName">Host name of remoter computer. Use '.' or 'localhost' or empty or null string for local Registry access. </param>
+    /// <param name="fullPath">PowerShell style registry key path and value name in one path string. 
+    /// <br><i>Example: HKLM:\Software\Vendor\ValueName</i></br>
+    /// <br><b>Note:</b> value names with '\' character are not supported as used for path separator</br>
+    /// </param>
+    /// <param name="defaultValue"></param>
     /// <returns></returns>
     public static object ReadRegistryValue(string computerName, string fullPath, object defaultValue = null)
     {
@@ -69,6 +188,12 @@ namespace Maximus.Library.Helpers
       }
     }
 
+    /// <summary>
+    /// Check registry key existence.
+    /// </summary>
+    /// <param name="computerName">Host name of remoter computer. Use '.' or 'localhost' or empty or null string for local Registry access. </param>
+    /// <param name="keyPath"></param>
+    /// <returns>True if the key exists, otherwise false.</returns>
     public static bool RegistryKeyExists(string computerName, string keyPath)
     {
       if (GetRegistryKey(computerName, keyPath) == null)
@@ -77,6 +202,15 @@ namespace Maximus.Library.Helpers
         return true;
     }
 
+    /// <summary>
+    /// Check registry value existence.
+    /// </summary>
+    /// <param name="computerName">Host name of remoter computer. Use '.' or 'localhost' or empty or null string for local Registry access. </param>
+    /// <param name="fullPath">PowerShell style registry key path and value name in one path string. 
+    /// <br><i>Example: HKLM:\Software\Vendor\ValueName</i></br>
+    /// <br><b>Note:</b> value names with '\' character are not supported as used for path separator</br>
+    /// </param>
+    /// <returns>True if registry value exists, otherwise false.</returns>
     public static bool RegistryValueExists(string computerName, string fullPath)
     {
       try
@@ -121,6 +255,13 @@ namespace Maximus.Library.Helpers
         return remoteRegistry;
     }
 
+    /// <summary>
+    /// Returns <seealso cref="RegistryKey"/> managed object for the referenced key.
+    /// </summary>
+    /// <param name="computerName"></param>
+    /// <param name="keyPath"></param>
+    /// <param name="writable"></param>
+    /// <returns></returns>
     public static RegistryKey GetRegistryKey(string computerName, string keyPath, bool writable = false)
     {
       RegistryHive hKey = ParseKeyPath(keyPath, out string pathName);
